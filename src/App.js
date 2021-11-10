64e19b2723c1d79e58862fd879202d1b95bbc02b
Dee Fialho <deefialho@gmail.com>
2021-11-09 22:05:16 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TopBar from "./components/topBar/TopBar";
+
+import Posts from "./components/posts/Posts"
+
+function App() {
+  return (
+    <>
+    <TopBar/>
+    <Posts/>
+    </>
+  );
+}
+
+export default App;
